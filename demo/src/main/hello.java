216906b4c523a0e7889d234c74ebdaef921081e7
gimdongha <d11210920@gmail.com>
2024-08-19 15:49:47 +0900

    123123

--- demo/src/main/hello.java
+++ demo/src/main/hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         int x;
         System.out.println(x);
-        
+        System.out.println("x = " + x);
     }
     public void helloString() {
 
